af0d8d377fd6fc986f3b5c34dfd06cd79f1eb065
shaday823-coder <zcarrillor823@inemkennedy.edu.co>
2026-06-03 17:29:39 -0500

    ola

--- app.py
+++ app.py
@@ -4,26 +4,36 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+@app.route("/informacion")
+def informacion():
+    datos = {
+        "aula": "215",
+        "profesor": "Henry Ortegon",
+        "horario": "Miercoles 16:45-18:10 | Jueves 12:30-14:20",
+        "objetivos": [
+            "Aprender Python basico",
+            "Entender Flask y aplicaciones web",
+            "Construir un portal web real"
+        ]
+    }
+    return render_template("informacion.html", **datos)
+
+@app.route("/recursos")
+def recursos():
+    enlaces = [
+        {"nombre": "Documentacion Flask", "url": "https://flask.palletsprojects.com"},
+        {"nombre": "Tutorial Python", "url": "https://docs.python.org"},
+        {"nombre": "GitHub del Profesor", "url": "https://github.com/hortegon"}
+    ]
+    return render_template("recursos.html", enlaces=enlaces)
+
+@app.route("/tareas")
+def tareas():
+    lista_tareas = [
+        {"numero": 1, "titulo": "Portal base", "fecha": "25/05/2026"},
+        {"numero": 2, "titulo": "Datos dinamicos", "fecha": "30/05/2026"},
+        {"numero": 3, "titulo": "Multiple paginas", "fecha": "05/06/2026"}
+    ]
+    return render_template("tareas.html", tareas=lista_tareas)
 
 
-@app.route("/")
-def inicio():
-     # Datos del portal
-    nombre_profesor = "Prof. Henry - Kyrbot Innovations"
-    email_profesor = "henry@kyrbot.com"
-    horario = "Miercoles 16:45-18:10 | Jueves 12:30-14:20"
-    aula = "215"
-    descripcion = "Aprenderemos Python, Flask y construiremos un portal web real"
-
-    return render_template(
-        "index.html",
-        profesor=nombre_profesor,
-        email=email_profesor,
-        horario=horario,
-        aula=aula,
-        descripcion=descripcion
-    )
-
-if __name__ == "__main__":
-
-    app.run(debug=True)
